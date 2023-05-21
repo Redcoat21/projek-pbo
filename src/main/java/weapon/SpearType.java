5afa4a5d9dd789085e7931cfa6b5d21d17aa26bd
Redcoat21 <89411491+Redcoat21@users.noreply.github.com>
2023-05-21 15:01:03 +0700

    Added explanation on what each spear do.

---
+++ src/main/java/weapon/SpearType.java
@@ -0,0 +1,17 @@
+package weapon;
+
+public enum SpearType {
+    /**
+     * Weakest spear type
+     */
+    IRON_SPEAR,
+    /**
+     * The only spear type to attack in crescent motion, similar to Sword
+     */
+    GLAIVE,
+    /**
+     * A Gigantic spear, attack in thrusting motion with a huge damage.
+     */
+    SPEAR_OF_THE_LORD,
+
+}
