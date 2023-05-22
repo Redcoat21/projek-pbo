3126817a3aac1263af4cae15b57d5f54246e03c9
Redcoat21 <89411491+Redcoat21@users.noreply.github.com>
2023-05-22 17:35:44 +0700

    Doing the same for the Player class

--- src/main/java/entities/Player.java
+++ src/main/java/entities/Player.java
@@ -7,11 +7,11 @@ public class Player extends Entities {
     private Weapon weapon;
 
     public Player(){
-        super(40, 400, 20, 20, 20, false, false, false, false, 5);
+        super(40, 400, 20, 20, 20, 5);
     }
 
     public Player(int x, int y){
-        super(x, y, 20, 20, 20, false, false, false, false, 5);
+        super(x, y, 20, 20, 20, 5);
     }
 
     @Override
