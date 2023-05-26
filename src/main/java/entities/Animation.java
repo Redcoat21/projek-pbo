0b8b2364553648fbdbea464876654931adacd545
Redcoat21 <89411491+Redcoat21@users.noreply.github.com>
2023-05-26 12:56:14 +0700

    Added docs, may continue later

--- src/main/java/entities/Animation.java
+++ src/main/java/entities/Animation.java
@@ -18,12 +18,21 @@ public class Animation {
         this.sprite = new ArrayList<>();
     }
 
+    /**
+     * Added an image into the animation list.
+     * @param entity
+     * @param pathToImage
+     */
     public void addImage(Entities entity, String pathToImage) {
         PImage image = Main.processing.loadImage(pathToImage);
         image.resize((int) entity.getWidth(), (int) entity.getHeight());
         this.sprite.add(image);
     }
 
+    /**
+     * Play the animation list.
+     * @param entity The entity that will be assigned to.
+     */
     public void playAnimation(Entities entity) {
         for(PImage image : this.sprite) {
             System.out.println(image.width);
