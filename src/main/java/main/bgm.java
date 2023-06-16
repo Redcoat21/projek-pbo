129f5dfb9d23019b82c89dfde974ccec6f738093
Jasson Putra <jassonputra23@gmaill.com>
2023-06-16 21:11:16 +0700

    bgm now depends on the mode

--- src/main/java/main/bgm.java
+++ src/main/java/main/bgm.java
@@ -7,10 +7,9 @@ public class bgm {
     static Clip clip;
     static URL[] bgmURL = new URL[30];
     public bgm(){
-        bgmURL[4]=getClass().getResource("/assets/sound/AI_Test_Kitchen_soothing_instrumental_music_to_help_me_focus.wav");
-        bgmURL[2]=getClass().getResource("/assets/sound/23. Accolade - Intro.wav");
-        bgmURL[1]=getClass().getResource("/assets/sound/AI_Test_Kitchen_dark_loopable.wav");
-        bgmURL[3]=getClass().getResource("/assets/sound/04. Awakening.wav");
+        bgmURL[2]=getClass().getResource("/assets/sound/AI_Test_Kitchen_soothing_instrumental_music_to_help_me_focus.wav");
+        bgmURL[3]=getClass().getResource("/assets/sound/AI_Test_Kitchen_intense_rock_battle.wav");
+        bgmURL[4]=getClass().getResource("/assets/sound/AI_Test_Kitchen_intense_battle_theme.wav");
     }
     public static void setFile(int i){
         try{
